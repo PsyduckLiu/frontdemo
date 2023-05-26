42bc3cf24ab05e49de67ff579c66e294e21133f7
PsyduckLiu <25915043@qq.com>
2023-05-26 16:12:36 +0800

    test

--- main_page.py
+++ main_page.py
@@ -1,14 +1,8 @@
 from scipy.interpolate import CubicSpline
 import streamlit as st
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import yfinance as yf
-import altair as alt
 import datetime
-from wallstreet import Stock, Call, Put
-from pandas_datareader import data as wb
-from scipy.stats import norm
 
 tab1, tab2, tab3 = st.tabs(["Parameters", "All Options Data", "Selected Options Data"])
 
